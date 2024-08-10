a1228720e7de0c33bac933ec643b842078b302fd
mb6ockatf <mdddmmmm@yandex.ru>
2024-08-10 13:35:47 +0300

    feat: initial commit

---
+++ src/main.c
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2024 Gordienkov Zakhar Yurievich aka @mb6ockatf
+ * This program is disributed under the terms of the GNU Affero General Public
+ * License version 3.0 or higher
+ * https://www.gnu.org/licenses/agpl-3.0.html
+ */
+#include <config.h>
+#include <stdio.h>
+
+int main (void)
+{
+	puts ("Hello World!");
+	puts ("This is " PACKAGE_STRING ".");
+	return 0;
+}
